95de86f085423780500920c7c525e06e73e22fca
kaustubh0201 <kaustubh1818@gmail.com>
2024-04-29 21:27:05 +0530

    added logic for the set command

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Readish
 An in-memory key-value database, cache and message broker.
+
+- <a href="https://redis.io/docs/latest/develop/reference/protocol-spec/">Redis serialization protocol specification</a>
---
+++ index.js
@@ -0,0 +1,36 @@
+const net  = require('net');
+const Parser = require('redis-parser');
+
+const store = {};
+
+// TCP Server
+const server = net.createServer(connection => {
+    console.log('Readish client connected.');
+
+    connection.on('data', data => {
+        const parser = new Parser({
+            returnReply: (reply) => {
+                const command = reply[0];
+                
+                switch (command) {
+                    case 'set': {
+                        const key = reply[1];
+                        const value = reply[2];
+                        store[key] = value;
+                        connection.write('+OK/r/n');
+                    }
+                    
+                    break;
+                }
+            },
+            returnError: (err) => {
+                console.log('=>', err);
+            }
+        });
+        parser.execute(data);
+        connection.write('+OK\r\n');
+    });
+
+});
+
+server.listen(8080, () => console.log(`Readish server running on port 8080.`));
